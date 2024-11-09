e9866ee4ef77fa99c69a798a10320c319ab9ce8b
Daphish <ivan.davila.2003@gmail.com>
2024-11-09 00:35:17 -0600

    Components folder added

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,17 @@
 import { createTheme } from "@mui/material";
 
-export const baseColors = {}
+export const baseColors = {
+    primary: {
+        /* #3A5DAE,
+        #B7BF10,
+        #ED8B00,
+        #000000,
+        #FFFFFF,
+        #DB4231, */
+    }
+}
 
 export const theme = createTheme({
-    
+    mode: "light",
+    palette: baseColors,
 })
