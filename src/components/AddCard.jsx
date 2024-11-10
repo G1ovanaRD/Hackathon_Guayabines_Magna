e3fe34b54a5994b92e2121988a87fcc05e49a71e
Rosalinda190 <RosalindaGamez@MacBook-Pro-de-Rosy.local>
2024-11-10 00:26:40 -0600

    add

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -61,7 +61,7 @@ export default function AddCard() {
           
         />
 
-        <Typography  variant="h6" sx={{ color: 'white', marginBottom: '8px' }}>Razón de baja: </Typography>
+        <Typography  variant="h6" sx={{ color: 'white', marginBottom: '8px' }}>Puesto: </Typography>
         <Input type="text" name="" id=""
             style={{
             borderRadius: '12px',
@@ -75,7 +75,7 @@ export default function AddCard() {
           
         />
 
-    <Typography  variant="h6" sx={{ color: 'white', marginBottom: '8px' }}>Nómina: </Typography>
+    <Typography  variant="h6" sx={{ color: 'white', marginBottom: '8px' }}>Área: </Typography>
         <Input type="text" name="" id=""
             style={{
             borderRadius: '12px',
@@ -89,14 +89,23 @@ export default function AddCard() {
           
         />
        
-    
-        <button style={{border:'solid 2px',borderColor:'#64dd17', width:'20%',borderRadius:'12px', padding:'0.4rem',margin:'1rem',color:'#ccc',background:'none',fontSize:"16px",cursor: 'pointer',
-        transition: 'background-color 0.3s ease',}}
-        onMouseEnter={(e) => (e.target.style.width='21%', e.target.style.boxShadow='0px -4px 10px rgba(255, 255, 255, 0.3), 0px 4px 10px rgba(255, 255, 255, 0.3)')}
-        onMouseLeave={(e) => (e.target.style.backgroundColor = 'transparent',e.target.style.color="#ccc", e.target.style.width='20%',e.target.style.boxShadow='none')}
-        onClick={handleLoginClick}>
-            Iniciar sesión
-        </button>
+       
+       <Box sx={{ display: 'flex', gap: '60px', padding: '30px' }}>
+         <button style={{border:'solid 2px',borderColor:'#64dd17', width:'100%',borderRadius:'12px', padding:'12px',color:'#ccc',background:'none',fontSize:"16px",cursor: 'pointer',
+          transition: 'background-color 0.3s ease'}}
+          onMouseEnter={(e) => (e.target.style.width='100%', e.target.style.boxShadow='0px -4px 10px rgba(255, 255, 255, 0.3), 0px 4px 10px rgba(255, 255, 255, 0.3)')}
+          onMouseLeave={(e) => (e.target.style.backgroundColor = 'transparent',e.target.style.color="#ccc", e.target.style.boxShadow='none')}
+          onClick={handleLoginClick}>
+            Cancelar
+          </button>
+          <button style={{border:'solid 2px',borderColor:'#64dd17', width:'100%',borderRadius:'12px', padding:'12px',color:'#ccc',background:'none',fontSize:"16px",cursor: 'pointer',
+          transition: 'background-color 0.3s ease'}}
+          onMouseEnter={(e) => (e.target.style.width='100%', e.target.style.boxShadow='0px -4px 10px rgba(255, 255, 255, 0.3), 0px 4px 10px rgba(255, 255, 255, 0.3)')}
+          onMouseLeave={(e) => (e.target.style.backgroundColor = 'transparent',e.target.style.color="#ccc", e.target.style.boxShadow='none')}
+          onClick={handleLoginClick}>
+              Agregar
+          </button>
+       </Box>
     </Box>
    );
 }
